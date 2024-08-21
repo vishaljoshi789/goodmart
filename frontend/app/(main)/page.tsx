fae79dbc9ea68c1de03b63d2f5d0dedbad5143f7
Vishal Joshi <vishalwebdev0987@gmail.com>
2024-08-21 19:56:20 +0530

    initialized working on home page

--- frontend/app/(main)/page.tsx
+++ frontend/app/(main)/page.tsx
@@ -1,6 +1,17 @@
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+
+import { ExclamationTriangleIcon } from "@radix-ui/react-icons";
+
 export default function Home() {
   return (
     <div>
+      <Alert variant="default">
+        <ExclamationTriangleIcon className="h-4 w-4" />
+        <AlertTitle>Error</AlertTitle>
+        <AlertDescription>
+          Your session has expired. Please log in again.
+        </AlertDescription>
+      </Alert>
       <h1>Home</h1>
       <p>Home page</p>
     </div>
