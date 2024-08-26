60f171998dbd3057ee9009a9beaf07ae4626a67c
Vishal Joshi <vishalwebdev0987@gmail.com>
2024-08-27 00:25:28 +0530

    Home page skeleton created

--- frontend/app/(main)/page.tsx
+++ frontend/app/(main)/page.tsx
@@ -3,6 +3,16 @@ import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
 import { useEffect, useState } from "react";
 import { TbAlertTriangle } from "react-icons/tb";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 
 interface Location {
   lat: number | null;
@@ -68,8 +78,350 @@ export default function Home() {
           {location.lat} {location.long}
         </div>
       )}
-      <h1>Home</h1>
-      <p>Home page</p>
+      <div className="lg:w-1/2 m-auto p-5">
+        <AspectRatio ratio={8 / 5} className="bg-gray-400">
+          {/* <Image src="..." alt="Image" className="rounded-md object-cover" /> */}
+        </AspectRatio>
+      </div>
+
+      <div className="py-5 px-10">
+        <h3 className="text-red-500 font-bold text-2xl font-serif">
+          Hot Selling Products
+        </h3>
+        <ScrollArea>
+          <div className="flex gap-3 py-5 justify-between">
+            <Card className="w-full md:w-56 flex md:flex-col items-center rounded-sm md:rounded-md h-fit">
+              <div className="md:w-full h-40 flex items-center bg-gray-100"></div>
+              <div className="md:w-full h-full">
+                <CardHeader className="p-2 pb-0">
+                  <CardTitle>Product Name</CardTitle>
+                  <CardDescription>Product Description</CardDescription>
+                </CardHeader>
+                <CardContent className="p-2 pt-2">
+                  <div className="flex gap-3">
+                    <s>₹2999</s>
+                    <b>₹1599</b>
+                  </div>
+                  <span className="text-red-500 text-lg">-50%</span>
+                </CardContent>
+                <CardFooter>
+                  <p>Product Category</p>
+                </CardFooter>
+              </div>
+            </Card>
+
+            <Card className="w-full md:w-56 flex md:flex-col items-center rounded-sm md:rounded-md h-fit">
+              <div className="md:w-full h-40 flex items-center bg-gray-100"></div>
+              <div className="md:w-full h-full">
+                <CardHeader className="p-2 pb-0">
+                  <CardTitle>Product Name</CardTitle>
+                  <CardDescription>Product Description</CardDescription>
+                </CardHeader>
+                <CardContent className="p-2 pt-2">
+                  <div className="flex gap-3">
+                    <s>₹2999</s>
+                    <b>₹1599</b>
+                  </div>
+                  <span className="text-red-500 text-lg">-50%</span>
+                </CardContent>
+                <CardFooter>
+                  <p>Product Category</p>
+                </CardFooter>
+              </div>
+            </Card>
+
+            <Card className="w-full md:w-56 flex md:flex-col items-center rounded-sm md:rounded-md h-fit">
+              <div className="md:w-full h-40 flex items-center bg-gray-100"></div>
+              <div className="md:w-full h-full">
+                <CardHeader className="p-2 pb-0">
+                  <CardTitle>Product Name</CardTitle>
+                  <CardDescription>Product Description</CardDescription>
+                </CardHeader>
+                <CardContent className="p-2 pt-2">
+                  <div className="flex gap-3">
+                    <s>₹2999</s>
+                    <b>₹1599</b>
+                  </div>
+                  <span className="text-red-500 text-lg">-50%</span>
+                </CardContent>
+                <CardFooter>
+                  <p>Product Category</p>
+                </CardFooter>
+              </div>
+            </Card>
+
+            <Card className="w-full md:w-56 flex md:flex-col items-center rounded-sm md:rounded-md h-fit">
+              <div className="md:w-full h-40 flex items-center bg-gray-100"></div>
+              <div className="md:w-full h-full">
+                <CardHeader className="p-2 pb-0">
+                  <CardTitle>Product Name</CardTitle>
+                  <CardDescription>Product Description</CardDescription>
+                </CardHeader>
+                <CardContent className="p-2 pt-2">
+                  <div className="flex gap-3">
+                    <s>₹2999</s>
+                    <b>₹1599</b>
+                  </div>
+                  <span className="text-red-500 text-lg">-50%</span>
+                </CardContent>
+                <CardFooter>
+                  <p>Product Category</p>
+                </CardFooter>
+              </div>
+            </Card>
+
+            <Card className="w-full md:w-56 flex md:flex-col items-center rounded-sm md:rounded-md h-fit">
+              <div className="md:w-full h-40 flex items-center bg-gray-100"></div>
+              <div className="md:w-full h-full">
+                <CardHeader className="p-2 pb-0">
+                  <CardTitle>Product Name</CardTitle>
+                  <CardDescription>Product Description</CardDescription>
+                </CardHeader>
+                <CardContent className="p-2 pt-2">
+                  <div className="flex gap-3">
+                    <s>₹2999</s>
+                    <b>₹1599</b>
+                  </div>
+                  <span className="text-red-500 text-lg">-50%</span>
+                </CardContent>
+                <CardFooter>
+                  <p>Product Category</p>
+                </CardFooter>
+              </div>
+            </Card>
+
+            <Card className="w-full md:w-56 flex md:flex-col items-center rounded-sm md:rounded-md h-fit">
+              <div className="md:w-full h-40 flex items-center bg-gray-100"></div>
+              <div className="md:w-full h-full">
+                <CardHeader className="p-2 pb-0">
+                  <CardTitle>Product Name</CardTitle>
+                  <CardDescription>Product Description</CardDescription>
+                </CardHeader>
+                <CardContent className="p-2 pt-2">
+                  <div className="flex gap-3">
+                    <s>₹2999</s>
+                    <b>₹1599</b>
+                  </div>
+                  <span className="text-red-500 text-lg">-50%</span>
+                </CardContent>
+                <CardFooter>
+                  <p>Product Category</p>
+                </CardFooter>
+              </div>
+            </Card>
+
+            <Card className="w-full md:w-56 flex md:flex-col items-center rounded-sm md:rounded-md h-fit">
+              <div className="md:w-full h-40 flex items-center bg-gray-100"></div>
+              <div className="md:w-full h-full">
+                <CardHeader className="p-2 pb-0">
+                  <CardTitle>Product Name</CardTitle>
+                  <CardDescription>Product Description</CardDescription>
+                </CardHeader>
+                <CardContent className="p-2 pt-2">
+                  <div className="flex gap-3">
+                    <s>₹2999</s>
+                    <b>₹1599</b>
+                  </div>
+                  <span className="text-red-500 text-lg">-50%</span>
+                </CardContent>
+                <CardFooter>
+                  <p>Product Category</p>
+                </CardFooter>
+              </div>
+            </Card>
+
+            <Card className="w-full md:w-56 flex md:flex-col items-center rounded-sm md:rounded-md h-fit">
+              <div className="md:w-full h-40 flex items-center bg-gray-100"></div>
+              <div className="md:w-full h-full">
+                <CardHeader className="p-2 pb-0">
+                  <CardTitle>Product Name</CardTitle>
+                  <CardDescription>Product Description</CardDescription>
+                </CardHeader>
+                <CardContent className="p-2 pt-2">
+                  <div className="flex gap-3">
+                    <s>₹2999</s>
+                    <b>₹1599</b>
+                  </div>
+                  <span className="text-red-500 text-lg">-50%</span>
+                </CardContent>
+                <CardFooter>
+                  <p>Product Category</p>
+                </CardFooter>
+              </div>
+            </Card>
+          </div>
+          <ScrollBar orientation="horizontal" />
+        </ScrollArea>
+      </div>
+      <div className="py-5 px-10">
+        <h3 className="text-red-500 font-bold text-2xl font-serif">
+          Deals of the Day
+        </h3>
+        <ScrollArea>
+          <div className="flex gap-3 py-5 justify-between">
+            <Card className="w-full md:w-56 flex md:flex-col items-center rounded-sm md:rounded-md h-fit">
+              <div className="md:w-full h-40 flex items-center bg-gray-100"></div>
+              <div className="md:w-full h-full">
+                <CardHeader className="p-2 pb-0">
+                  <CardTitle>Product Name</CardTitle>
+                  <CardDescription>Product Description</CardDescription>
+                </CardHeader>
+                <CardContent className="p-2 pt-2">
+                  <div className="flex gap-3">
+                    <s>₹2999</s>
+                    <b>₹1599</b>
+                  </div>
+                  <span className="text-red-500 text-lg">-50%</span>
+                </CardContent>
+                <CardFooter>
+                  <p>Product Category</p>
+                </CardFooter>
+              </div>
+            </Card>
+
+            <Card className="w-full md:w-56 flex md:flex-col items-center rounded-sm md:rounded-md h-fit">
+              <div className="md:w-full h-40 flex items-center bg-gray-100"></div>
+              <div className="md:w-full h-full">
+                <CardHeader className="p-2 pb-0">
+                  <CardTitle>Product Name</CardTitle>
+                  <CardDescription>Product Description</CardDescription>
+                </CardHeader>
+                <CardContent className="p-2 pt-2">
+                  <div className="flex gap-3">
+                    <s>₹2999</s>
+                    <b>₹1599</b>
+                  </div>
+                  <span className="text-red-500 text-lg">-50%</span>
+                </CardContent>
+                <CardFooter>
+                  <p>Product Category</p>
+                </CardFooter>
+              </div>
+            </Card>
+
+            <Card className="w-full md:w-56 flex md:flex-col items-center rounded-sm md:rounded-md h-fit">
+              <div className="md:w-full h-40 flex items-center bg-gray-100"></div>
+              <div className="md:w-full h-full">
+                <CardHeader className="p-2 pb-0">
+                  <CardTitle>Product Name</CardTitle>
+                  <CardDescription>Product Description</CardDescription>
+                </CardHeader>
+                <CardContent className="p-2 pt-2">
+                  <div className="flex gap-3">
+                    <s>₹2999</s>
+                    <b>₹1599</b>
+                  </div>
+                  <span className="text-red-500 text-lg">-50%</span>
+                </CardContent>
+                <CardFooter>
+                  <p>Product Category</p>
+                </CardFooter>
+              </div>
+            </Card>
+
+            <Card className="w-full md:w-56 flex md:flex-col items-center rounded-sm md:rounded-md h-fit">
+              <div className="md:w-full h-40 flex items-center bg-gray-100"></div>
+              <div className="md:w-full h-full">
+                <CardHeader className="p-2 pb-0">
+                  <CardTitle>Product Name</CardTitle>
+                  <CardDescription>Product Description</CardDescription>
+                </CardHeader>
+                <CardContent className="p-2 pt-2">
+                  <div className="flex gap-3">
+                    <s>₹2999</s>
+                    <b>₹1599</b>
+                  </div>
+                  <span className="text-red-500 text-lg">-50%</span>
+                </CardContent>
+                <CardFooter>
+                  <p>Product Category</p>
+                </CardFooter>
+              </div>
+            </Card>
+
+            <Card className="w-full md:w-56 flex md:flex-col items-center rounded-sm md:rounded-md h-fit">
+              <div className="md:w-full h-40 flex items-center bg-gray-100"></div>
+              <div className="md:w-full h-full">
+                <CardHeader className="p-2 pb-0">
+                  <CardTitle>Product Name</CardTitle>
+                  <CardDescription>Product Description</CardDescription>
+                </CardHeader>
+                <CardContent className="p-2 pt-2">
+                  <div className="flex gap-3">
+                    <s>₹2999</s>
+                    <b>₹1599</b>
+                  </div>
+                  <span className="text-red-500 text-lg">-50%</span>
+                </CardContent>
+                <CardFooter>
+                  <p>Product Category</p>
+                </CardFooter>
+              </div>
+            </Card>
+
+            <Card className="w-full md:w-56 flex md:flex-col items-center rounded-sm md:rounded-md h-fit">
+              <div className="md:w-full h-40 flex items-center bg-gray-100"></div>
+              <div className="md:w-full h-full">
+                <CardHeader className="p-2 pb-0">
+                  <CardTitle>Product Name</CardTitle>
+                  <CardDescription>Product Description</CardDescription>
+                </CardHeader>
+                <CardContent className="p-2 pt-2">
+                  <div className="flex gap-3">
+                    <s>₹2999</s>
+                    <b>₹1599</b>
+                  </div>
+                  <span className="text-red-500 text-lg">-50%</span>
+                </CardContent>
+                <CardFooter>
+                  <p>Product Category</p>
+                </CardFooter>
+              </div>
+            </Card>
+
+            <Card className="w-full md:w-56 flex md:flex-col items-center rounded-sm md:rounded-md h-fit">
+              <div className="md:w-full h-40 flex items-center bg-gray-100"></div>
+              <div className="md:w-full h-full">
+                <CardHeader className="p-2 pb-0">
+                  <CardTitle>Product Name</CardTitle>
+                  <CardDescription>Product Description</CardDescription>
+                </CardHeader>
+                <CardContent className="p-2 pt-2">
+                  <div className="flex gap-3">
+                    <s>₹2999</s>
+                    <b>₹1599</b>
+                  </div>
+                  <span className="text-red-500 text-lg">-50%</span>
+                </CardContent>
+                <CardFooter>
+                  <p>Product Category</p>
+                </CardFooter>
+              </div>
+            </Card>
+
+            <Card className="w-full md:w-56 flex md:flex-col items-center rounded-sm md:rounded-md h-fit">
+              <div className="md:w-full h-40 flex items-center bg-gray-100"></div>
+              <div className="md:w-full h-full">
+                <CardHeader className="p-2 pb-0">
+                  <CardTitle>Product Name</CardTitle>
+                  <CardDescription>Product Description</CardDescription>
+                </CardHeader>
+                <CardContent className="p-2 pt-2">
+                  <div className="flex gap-3">
+                    <s>₹2999</s>
+                    <b>₹1599</b>
+                  </div>
+                  <span className="text-red-500 text-lg">-50%</span>
+                </CardContent>
+                <CardFooter>
+                  <p>Product Category</p>
+                </CardFooter>
+              </div>
+            </Card>
+          </div>
+          <ScrollBar orientation="horizontal" />
+        </ScrollArea>
+      </div>
     </div>
   );
 }
