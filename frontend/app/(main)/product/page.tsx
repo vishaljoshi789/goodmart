df18db5350ddd2a285fa6f23469c4f5b977259c0
Vishal Joshi <vishalwebdev0987@gmail.com>
2024-09-01 20:30:28 +0530

    added video to the carousel

--- frontend/app/(main)/product/page.tsx
+++ frontend/app/(main)/product/page.tsx
@@ -5,16 +5,31 @@ import { useContext, useEffect, useState } from "react";
 import { GMContext } from "@/app/(utils)/context/GMContext";
 import { toast } from "sonner";
 import NotFoundPage from "@/components/web/404";
+import Image from "next/image";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { type CarouselApi } from "@/components/ui/carousel";
+import { ScrollArea } from "@radix-ui/react-scroll-area";
+import { ScrollBar } from "@/components/ui/scroll-area";
+import { Card, CardContent } from "@/components/ui/card";
 
 export default function Product() {
   let id = useSearchParams().get("id");
-  let api = useAxios();
+  const [api, setApi] = useState<CarouselApi>();
   let { baseURL } = useContext(GMContext);
   let [product, setProduct] = useState<any>({});
+  let [activeImage, setActiveImage] = useState(0);
   let getProduct = async () => {
     let response = await fetch(`${baseURL}/getProduct/${id}/`);
     if (response.status == 200) {
       let data = await response.json();
+      console.log(data);
+      setProduct(data);
     }
     if (response.status == 404) {
       toast.error("Product Not Found!");
@@ -22,25 +37,100 @@ export default function Product() {
     }
   };
   useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    api.on("select", () => {
+      setActiveImage(api.selectedScrollSnap());
+    });
+  }, [api]);
+
+  useEffect(() => {
     getProduct();
   }, []);
   return (
     <div>
       {product ? (
         <section className="text-gray-600 body-font overflow-hidden">
-          <div className="container px-5 py-24 mx-auto">
-            <div className="lg:w-4/5 mx-auto flex flex-wrap">
-              <img
-                alt="ecommerce"
-                className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
-                src="https://dummyimage.com/400x400"
-              />
+          <div className="px-5 py-24 mx-auto">
+            <div className="mx-auto flex flex-wrap justify-evenly">
+              <div className="flex flex-col gap-5 items-center sticky z-10 top-0 shadow-md">
+                <Carousel
+                  className="w-full xl:max-w-2xl lg:max-w-md md:max-w-sm"
+                  setApi={setApi}
+                >
+                  <CarouselContent>
+                    {product.images &&
+                      product.images.map((image: any, index: any) => (
+                        <CarouselItem key={index}>
+                          <Card>
+                            <CardContent className="flex items-center justify-center p-5 aspect-square">
+                              <Image
+                                alt={product.name}
+                                height={0}
+                                width={0}
+                                sizes="100vw"
+                                className="w-full h-full object-contain object-center"
+                                src={`${baseURL}${image["image"]}`}
+                              />
+                            </CardContent>
+                          </Card>
+                        </CarouselItem>
+                      ))}
+                    {product.video && (
+                      <CarouselItem>
+                        <Card>
+                          <CardContent className="flex items-center justify-center p-5 aspect-square">
+                            <video
+                              className="w-full h-full object-contain object-center"
+                              src={`${baseURL}${product["video"]}`}
+                              controls
+                            />
+                          </CardContent>
+                        </Card>
+                      </CarouselItem>
+                    )}
+                  </CarouselContent>
+                </Carousel>
+                <ScrollArea className="bg-white">
+                  <div className="flex items-center justify-evenly gap-2 p-3">
+                    {product.images &&
+                      product.images.map((item: any, index: number) => (
+                        <Image
+                          onClick={() => api?.scrollTo(index)}
+                          alt="product-img"
+                          height={0}
+                          width={0}
+                          sizes="100vw"
+                          className={`w-16 h-16 object-contain object-center rounded ${
+                            index == activeImage && `border-black border-2`
+                          } `}
+                          src={`${baseURL}${item["image"]}`}
+                          key={item.id}
+                        />
+                      ))}
+                    {product.video && (
+                      <video
+                        className={`w-16 h-16 object-contain object-center rounded ${
+                          activeImage == product.images.length &&
+                          `border-black border-2`
+                        } `}
+                        onClick={() => api?.scrollTo(product.images.length)}
+                      >
+                        <source src={`${baseURL}${product["video"]}`} />
+                      </video>
+                    )}
+                  </div>
+                  <ScrollBar orientation="horizontal" />
+                </ScrollArea>
+              </div>
               <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
                 <h2 className="text-sm title-font text-gray-500 tracking-widest">
-                  BRAND NAME
+                  {product.brand}
                 </h2>
                 <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
-                  The Catcher in the Rye
+                  {product.name}
                 </h1>
                 <div className="flex mb-4">
                   <span className="flex items-center">
@@ -140,50 +230,17 @@ export default function Product() {
                     </a>
                   </span>
                 </div>
-                <p className="leading-relaxed">
-                  Fam locavore kickstarter distillery. Mixtape chillwave tumeric
-                  sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo
-                  juiceramps cornhole raw denim forage brooklyn. Everyday carry
-                  +1 seitan poutine tumeric. Gastropub blue bottle austin
-                  listicle pour-over, neutra jean shorts keytar banjo tattooed
-                  umami cardigan.
-                </p>
-                <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
-                  <div className="flex">
-                    <span className="mr-3">Color</span>
-                    <button className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
-                    <button className="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
-                    <button className="border-2 border-gray-300 ml-1 bg-indigo-500 rounded-full w-6 h-6 focus:outline-none"></button>
-                  </div>
-                  <div className="flex ml-6 items-center">
-                    <span className="mr-3">Size</span>
-                    <div className="relative">
-                      <select className="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
-                        <option>SM</option>
-                        <option>M</option>
-                        <option>L</option>
-                        <option>XL</option>
-                      </select>
-                      <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
-                        <svg
-                          fill="none"
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          className="w-4 h-4"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M6 9l6 6 6-6"></path>
-                        </svg>
-                      </span>
-                    </div>
-                  </div>
-                </div>
+                <p className="leading-relaxed">{product.description}</p>
+                <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5"></div>
                 <div className="flex">
-                  <span className="title-font font-medium text-2xl text-gray-900">
-                    $58.00
-                  </span>
+                  <div className="flex gap-5">
+                    <s className="title-font font-medium text-2xl text-red-500">
+                      ₹{product.mrp}
+                    </s>
+                    <span className="title-font font-medium text-2xl text-gray-900">
+                      ₹{product.offer_price}
+                    </span>
+                  </div>
                   <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
                     Button
                   </button>
