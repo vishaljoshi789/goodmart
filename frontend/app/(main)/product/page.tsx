cf609ef96a3a3a0822fbc7f40b18f501647fd704
Vishal Joshi <vishalwebdev0987@gmail.com>
2024-09-02 16:30:07 +0530

    added share in product page

--- frontend/app/(main)/product/page.tsx
+++ frontend/app/(main)/product/page.tsx
@@ -10,13 +10,13 @@ import {
   Carousel,
   CarouselContent,
   CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
 } from "@/components/ui/carousel";
 import { type CarouselApi } from "@/components/ui/carousel";
 import { ScrollArea } from "@radix-ui/react-scroll-area";
 import { ScrollBar } from "@/components/ui/scroll-area";
 import { Card, CardContent } from "@/components/ui/card";
+import { FaShare } from "react-icons/fa";
+import { Button } from "@/components/ui/button";
 
 export default function Product() {
   let id = useSearchParams().get("id");
@@ -36,6 +36,19 @@ export default function Product() {
       setProduct(null);
     }
   };
+
+  let handleShare = async () => {
+    try {
+      await navigator.share({
+        title: "GOODMART",
+        text: product.name,
+        url: window.location.href,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   useEffect(() => {
     if (!api) {
       return;
@@ -196,42 +209,12 @@ export default function Product() {
                     <span className="text-gray-600 ml-3">4 Reviews</span>
                   </span>
                   <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
-                    <a className="text-gray-500">
-                      <svg
-                        fill="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        className="w-5 h-5"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                      </svg>
-                    </a>
-                    <a className="text-gray-500">
-                      <svg
-                        fill="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        className="w-5 h-5"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                      </svg>
-                    </a>
-                    <a className="text-gray-500">
-                      <svg
-                        fill="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        className="w-5 h-5"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                      </svg>
-                    </a>
+                    <Button
+                      className="bg-white hover:bg-white text-blue-500 hover:text-blue-700 "
+                      onClick={handleShare}
+                    >
+                      <FaShare className="text-2xl" />
+                    </Button>
                   </span>
                 </div>
                 <p className="leading-relaxed">{product.description}</p>
