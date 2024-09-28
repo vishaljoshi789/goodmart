8359437abbbccf469bed2b73a6a5259d452fe092
Vishal Joshi <vishalwebdev0987@gmail.com>
2024-09-29 02:24:58 +0530

    modified models

--- backend/mainapp/models.py
+++ backend/mainapp/models.py
@@ -99,6 +99,7 @@ class Address(models.Model):
     state = models.CharField(max_length=100, blank=True, null=True)
     pin = models.CharField(max_length=6, blank=True, null=True)
     phone = models.CharField(max_length=15, blank=True, null=True)
+    co_ordinates = models.CharField(max_length=150, blank=True, null=True)
     alternate_phone = models.CharField(max_length=15, blank=True, null=True)
     added_on = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     modify_on = models.DateTimeField(auto_now=True, blank=True, null=True)
@@ -121,7 +122,8 @@ class Product_Category(models.Model):
 class Vendor_Detail(models.Model):
     firm = models.CharField(max_length=100, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
-    category = models.ForeignKey(Product_Category, on_delete=models.CASCADE, blank=True, null=True)
+    category = models.ForeignKey(Product_Category, on_delete=models.CASCADE, blank=True, null=True, related_name='shop')
+    primary_category = models.ForeignKey(Product_Category, on_delete=models.CASCADE, blank=True, null=True, related_name='primary_shop')
     user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True, related_name='vendor')
     photograph = models.ImageField(upload_to=vendor_directory_path, blank=True, null=True)
     gst = models.CharField(max_length=20, blank=True, null=True)
@@ -133,29 +135,20 @@ class Vendor_Detail(models.Model):
     aadhar_back_image = models.ImageField(upload_to=vendor_directory_path, blank=True, null=True)
     address = models.ForeignKey(Address, on_delete=models.CASCADE, blank=True, null=True)
     qr = models.ImageField(upload_to=vendor_directory_path, blank=True, null=True)
+    vendor_visiblity = models.BooleanField(default=True)
+    admin_visiblity = models.BooleanField(default=False)
     status = models.CharField(max_length=20, blank=True, null=True, choices=(("Pending", "Pending"), ("Approved", "Approved"), ("Rejected", "Rejected")), default="Pending")
     status_message = models.CharField(max_length=100, blank=True, null=True, default="Status is in Pending State")
+    cash_on_delivery = models.BooleanField(default=False)
+    featured = models.BooleanField(default=False)
     added_on = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     modify_on = models.DateTimeField(auto_now=True, blank=True, null=True)
-
-class Shop(models.Model):
-    vendor = models.ForeignKey(Vendor_Detail, on_delete=models.CASCADE, blank=True, null=True, related_name='shop')
-    name = models.CharField(max_length=100, blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
-    address = models.ForeignKey(Address, on_delete=models.CASCADE, blank=True, null=True)
-    phone = models.CharField(max_length=15, blank=True, null=True)
-    alternate_phone = models.CharField(max_length=15, blank=True, null=True)
-    email = models.EmailField(blank=True, null=True)
     image1 = models.ImageField(upload_to=vendor_directory_path, blank=True, null=True)
     image2 = models.ImageField(upload_to=vendor_directory_path, blank=True, null=True)
     image3 = models.ImageField(upload_to=vendor_directory_path, blank=True, null=True)
-    status = models.BooleanField(default=True)
-    added_on = models.DateTimeField(auto_now_add=True, blank=True, null=True)
-    modify_on = models.DateTimeField(auto_now=True, blank=True, null=True)
-    modify_by = models.IntegerField(blank=True, null=True)
+    logo = models.ImageField(upload_to=vendor_directory_path, blank=True, null=True)
+    free_shipping_above = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
     
-    def __str__(self):
-        return self.name
 
     
 class Product_Brand(models.Model):
@@ -184,23 +177,23 @@ class Product(models.Model):
     tags = models.TextField(blank=True, null=True)
     image = models.ImageField(upload_to=vendor_directory_path, blank=True, null=True)
     video = models.FileField(upload_to=vendor_directory_path, blank=True, null=True)
-    # item_type = models.IntegerField(blank=True, null=True)
+    hsn = models.CharField(max_length=100, blank=True, null=True)
+    tax = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True)
+    stock = models.IntegerField(blank=True, null=True)
+    item_type = models.CharField(max_length=10, blank=True, null=True, choices=(("Packed", "Packed"), ("Open", "Open")))
+    point = models.IntegerField(default=0, blank=True, null=True)
+    company_id = models.ForeignKey(Vendor_Detail, on_delete=models.CASCADE, blank=True, null=True)
     # position = models.IntegerField(blank=True, null=True)
     # unit = models.CharField(max_length=100, blank=True, null=True)
-    # stock = models.IntegerField(blank=True, null=True)
     # rating = models.IntegerField(blank=True, null=True)
-    # point = models.IntegerField(default=1, blank=True, null=True)
     # modify_by = models.IntegerField(blank=True, null=True)
     # shipping = models.DecimalField(max_digits=10, decimal_places=3, blank=True, null=True)
-    # tax = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True)
     # product_id = models.CharField(max_length=100, blank=True, null=True)
     # product_added_type = models.IntegerField(default=1, blank=True, null=True)
-    # company_id = models.IntegerField(blank=True, null=True)
     # target = models.CharField(max_length=100, default='repurchase', blank=True, null=True)
     # capping = models.IntegerField(blank=True, null=True)
     # ratio = models.IntegerField(blank=True, null=True)
     # pairs = models.IntegerField(blank=True, null=True)
-    # hsn = models.CharField(max_length=100, blank=True, null=True)
 
 
     def __str__(self):
@@ -249,4 +242,12 @@ class Points(models.Model):
 
 class Setting(models.Model):
     registration_points = models.PositiveIntegerField(null=True, blank=True, default=0)
+
+class ShippingCharges(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    pincode = models.CharField(max_length=6, null=True, blank=True)
+    charges = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True)
+    added_on = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    modify_on = models.DateTimeField(auto_now=True, null=True, blank=True)
+
 
