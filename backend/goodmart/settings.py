63e96d0df369ad0547dd0ca18babbcb40a8cf04f
Vishal Joshi <vishalwebdev0987@gmail.com>
2024-11-27 23:30:32 +0530

    deploy

--- backend/goodmart/settings.py
+++ backend/goodmart/settings.py
@@ -87,12 +87,13 @@ SIMPLE_JWT = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-     "corsheaders.middleware.CorsMiddleware",
+    
 ]
 
 ROOT_URLCONF = 'goodmart.urls'
