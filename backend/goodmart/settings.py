e0a78f796b0cffb6e5e391910215f8d0408a5988
Vishal Joshi <vishalwebdev0987@gmail.com>
2025-01-24 00:12:31 +0530

    added postgres database

--- backend/goodmart/settings.py
+++ backend/goodmart/settings.py
@@ -136,12 +136,24 @@ CORS_ALLOWED_ORIGINS = [
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
+if LOGGING:
+    DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('POSTGRES_DB_NAME'),
+        'USER': config('POSTGRES_DB_USER'),
+        'PASSWORD': config('POSTGRES_DB_PASSWORD'),
+        'HOST': config('POSTGRES_DB_HOST'),
+        'PORT': config('POSTGRES_DB_PORT'),
     }
 }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
