1b2336174aa1413b65747d4e73a4cd4782f1e442
Vishal Joshi <vishalwebdev0987@gmail.com>
2025-02-12 07:20:49 +0530

    added ci/cd

--- backend/goodmart/settings.py
+++ backend/goodmart/settings.py
@@ -139,11 +139,11 @@ if LOGGING:
     DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),  # Database name
-        'USER': config('DB_USER'),  # Database user
-        'PASSWORD': config('DB_PASSWORD'),  # Database password
-        'HOST': config('DB_HOST'),  # Database host (or IP address)
-        'PORT': config('DB_PORT'),  # Database port (default: 5432)
+        'NAME': config('DB_NAME'), 
+        'USER': config('DB_USER'),  
+        'PASSWORD': config('DB_PASSWORD'), 
+        'HOST': config('DB_HOST'),  
+        'PORT': config('DB_PORT'),  
         }
     }
 else:
